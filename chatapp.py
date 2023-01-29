c0e91fd8f4d089230ff7a741259d79a8ad159467
john kariuki <johnkariukinjoroge5@gmail.com>
2023-01-29 17:07:03 +0300

    first commit

--- chatapp.py
+++ chatapp.py
@@ -4,7 +4,13 @@ def chat():
     print("Chatbot: Hello, I'm a chatbot. How can I help you today?\n")
     while True:
         user_input = input("You: ")
-        if user_input.lower() in ["quit", "q", "exit"]:
+        if user_input.lower() in ["hi", "hello", "hey"]:
+            print("Chatbot: Hello! How can I help you today?")
+        elif user_input.lower() in ["how are you", "how are you doing"]:
+            print("Chatbot: I'm doing well, thank you for asking! How about you?")
+        elif user_input.lower() in ["what's your name", "what is your name"]:
+            print("Chatbot: My name is Chatbot, nice to meet you!")
+        elif user_input.lower() in ["quit", "q", "exit"]:
             print("Chatbot: Goodbye! Have a great day.")
             break
         else:
